efe1b3162516507bce2bf270d252724b37c355d1
Katalina Palma <kata_92@hotmail.com>
2021-08-02 16:37:55 +0200

    a mess

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-class Index {
-    theBank() {
-        this.getLoanBtn = document.getElementById("getALoanBtn")
-        this.getLoanBtn.addEventListener("click", function() {
-            console.log('hej');
+// window.onload = function () {
+//         this.getLoanBtn = document.getElementById("getALoanBtn")
+//         this.getLoanBtn.addEventListener("click", function() {
+//             console.log('click click click');
+//         })
+
+
+// }
+
+function clickGetLoan() {
+    console.log('click click click');
+}
+
+function clickBank() {
+    console.log('bank bank bank')
+}
+
+function clickWork() {
+    console.log('work work work')
+}
+
+function getData() {
+    fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
+    .then(function(response) {
+        return response.json();
         })
-    }
-
-
-    test() {
-        btn = document.getElementById('getALoanBtn');   
-        this.btn.addEventListener("click", getLoan, once);    
-    }
- 
-    getLoan(e) {
-        console.log('click click')
-    }
+        .then(function(data) {
+            console.log(data);
+        })
+        .catch(function() {
+            console.log("Booo");
+        });
 }
